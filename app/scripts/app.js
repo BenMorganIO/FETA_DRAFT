53c53c92f2bcfdb55f43f59ed045eb487e4f6f6d
Ben A. Morgan <benjaminamorgan@hotmail.com>
2014-02-14 20:48:06 -0800

    set up bootstrap, glossary, and initial template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,25 @@ angular.module('fetaApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+      })
+      .when('/expressions', {
+        templateUrl: 'views/expressions.html',
+        controller: 'ExprCtrl'
+      })
+      .when('/exercises', {
+        templateUrl: 'views/exercises.html',
+        controller: 'ExerCtrl'
+      })
+      .when('/glossary', {
+        templateUrl: 'views/glossary.html',
+        controller: 'GlosCtrl'
+      })
+      .when('/settings', {
+        templateUrl: 'views/settings.html',
+        controller: 'SetsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
