ddebcdbf78ddf1941728a62e678de32dae33820d
Ben A. Morgan <benjaminamorgan@hotmail.com>
2014-02-14 22:20:30 -0800

    set module to feta variable

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,38 @@
 'use strict';
 
-angular.module('fetaApp', [
+var feta = angular.module('fetaApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-      })
-      .when('/expressions', {
-        templateUrl: 'views/expressions.html',
-        controller: 'ExprCtrl'
-      })
-      .when('/exercises', {
-        templateUrl: 'views/exercises.html',
-        controller: 'ExerCtrl'
-      })
-      .when('/glossary', {
-        templateUrl: 'views/glossary.html',
-        controller: 'GlosCtrl'
-      })
-      .when('/settings', {
-        templateUrl: 'views/settings.html',
-        controller: 'SetsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+feta.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+    })
+    .when('/expressions', {
+      templateUrl: 'views/expressions.html',
+      controller: 'ExprCtrl'
+    })
+    .when('/exercises', {
+      templateUrl: 'views/exercises.html',
+      controller: 'ExerCtrl'
+    })
+    .when('/settings', {
+      templateUrl: 'views/settings.html',
+      controller: 'SetsCtrl'
+    })
+    .when('/glossary', {
+      templateUrl: 'views/glossary.html',
+      controller: 'GlossaryCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
